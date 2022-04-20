574c4e8f2b3debd8f99b2dbc26e63c5fbb573da6
Irene Njeru <irenenjeru8@gmail.com>
2022-04-20 11:49:18 +0300

    task completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+function findMatching(driver1, param){
+    let driveList = [];
+
+    driver1.forEach(name => {
+        if(name.toUpperCase() == param.toUpperCase()){
+            driveList.push(name)
+        }
+    });
+
+    return driveList;
+}
+
+function fuzzyMatch(param1, param2){
+    let fuzzyDrivers = [];
+    param1.forEach(name => {
+        if(name.charAt(0) === param2.charAt(0)){
+            fuzzyDrivers.push(name);
+        }
+    })
+    return fuzzyDrivers
+}
+
+function matchName(param1, param2){
+    let driver = [];
+    for(let names of param1){
+        if(names.name === param2){
+            driver.push(names);
+        }
+    }
+    return driver;
+}
+
+console.log(fuzzyMatch(drivers, "San"))
